bd06608e19570ea26543a43e82685af04ea44491
Tania <tanyafi2010@gmail.com>
2023-11-05 13:33:38 +0200

    qwed

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from PyQt5.QtWidgets import QApplication,QMessageBox#імпорт всього що нада
 from random import shuffle,choice#імпорт з рандому
-from data import qwList
+
+
 
 app = QApplication([])#створюємо додаток
 
+from data import qwList, Qwestion
 from main_win import*#імпортуємо з мейну все
-
+from edit_win import*
 choce_ans = ""
 qw = ""
+def stat():
+    count = 0
+    count_r = 0
+    good = 0
+    for q in qwList:
+        count+=q.count
+        count_r+=q.count_right
+    if count!=0:    
+        good = (count_r/count)*100  
+    else:
+        good=0   
+    lbl_count.setText(f"кількість відповідей {count}")  
+    lbl_count_right.setText(f"кількість правельних відповідей {count_r}")
+    lbl_good.setText(f"процент правельних відповідей {good}")
 
 def new_qw():
     global qw
@@ -55,13 +71,48 @@ def check_ans():
         box_result.hide()
         box_ans.show()
         
-        
+def show_menu():
+    stat()
+    win.hide()
+    win_edit.show()        
+   
+def show_test():
+    win_edit.hide()
+    win.show()
+def clear():
+    edit_ans.clear()
+    edit_qw.clear()
+    edit_wrg1.clear()
+    edit_wrg2.clear()
+    edit_wrg3.clear()
+
+
+def add_qw():
+    qw_text = edit_qw.text()
+    ans_text = edit_ans.text()
+    wg1_text = edit_wrg1.text()
+    wg2_text = edit_wrg2.text()
+    wg3_text = edit_wrg2.text()
+    if qw!="" and ans_text!="" and wg1_text!= "" and wg2_text!="" and wg3_text!="":
+        q = Qwestion(qw_text, ans_text, wg1_text, wg2_text, wg3_text)
+        qwList.append(q)
+        clear()
+    else:
+        mess_wg = QMessageBox()
+        mess_wg.setText("ДОПИШИ")
+        mess_wg.show()
+        mess_wg.exec()
 
 def click_ans(rbn):
     global choce_ans
     choce_ans = rbn.text()
 
 new_qw()
+
+btn_menu.clicked.connect(show_menu)
+btn_back.clicked.connect(show_test)
+btn_clear.clicked.connect(clear)
+btn_add_qw.clicked.connect(add_qw)
 
 rbn_group.buttonClicked.connect(click_ans)
 btn_check.clicked.connect(check_ans)
